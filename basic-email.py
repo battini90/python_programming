a4422286a9807935b293b37541d79a35bb3e72e6
battini90 <koushikbattini@gmail.com>
2018-02-18 19:06:25 -0600

    updated file

--- basic-email.py
+++ basic-email.py
@@ -2,7 +2,7 @@ import smtplib
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
-server.login("tinku.is.koushik@gmail.com", "9441124276")
+server.login("tinku.is.koushik@gmail.com", "")
 
 msg = "Hello Koushik, its from your script"
 server.sendmail("tinku.is.koushik@gmail.com", "koushikbattini@gmail.com", msg)
